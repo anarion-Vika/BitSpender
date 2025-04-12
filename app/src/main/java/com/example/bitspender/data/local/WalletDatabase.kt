dcddc67afa80f8aecb43ee08ea2eb501e8bbd579
Viktoriia Sievierina <viktoriia.domukhovska@trackimo.com>
2025-04-12 18:19:07 +0300

    code refactoring

--- app/src/main/java/com/example/bitspender/data/local/WalletDatabase.kt
+++ app/src/main/java/com/example/bitspender/data/local/WalletDatabase.kt
@@ -5,15 +5,19 @@ import androidx.room.RoomDatabase
 import com.example.bitspender.data.models.BtcRateEntity
 import com.example.bitspender.data.models.TransactionEntity
 
-abstract class WalletDatabase {
-    @Database(
-        entities = [TransactionEntity::class, BtcRateEntity::class],
-        version = 1
-    )
+
+@Database(
+    entities =
+        [TransactionEntity::class,
+            BtcRateEntity::class],
+    version = 1
+)
 
 
-    abstract class WalletDatabase : RoomDatabase() {
-        abstract fun transactionDao(): TransactionDao
-        abstract fun btcRateDao(): BtcRateDao
-    }
+abstract class WalletDatabase : RoomDatabase() {
+
+    abstract fun transactionDao(): TransactionDao
+
+    abstract fun btcRateDao(): BtcRateDao
+
 }
