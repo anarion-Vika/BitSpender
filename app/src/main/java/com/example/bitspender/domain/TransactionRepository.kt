ac123b59b60d8856e46c70dc955fffa5d9ce3513
Viktoriia Sievierina <viktoriia.domukhovska@trackimo.com>
2025-04-14 12:06:37 +0300

    Added repository interfaces and use cases for transactions and BTC rate

--- app/src/main/java/com/example/bitspender/domain/TransactionRepository.kt
+++ app/src/main/java/com/example/bitspender/domain/TransactionRepository.kt
@@ -1,6 +1,13 @@
 package com.example.bitspender.domain
 
+import com.example.bitspender.domain.models.TransactionModel
+import kotlinx.coroutines.flow.Flow
+
 interface TransactionRepository {
 
+    suspend fun addTransaction(transaction: TransactionModel): Result<Unit>
 
+    fun getTransactions(): Flow<List<TransactionModel>>
+
+    fun getBalance(): Flow<Float>
 }
