682201282580cd134b223785a10522aec0a418d1
Viktoriia Sievierina <viktoriia.domukhovska@trackimo.com>
2025-04-16 14:03:06 +0300

    Added logic to MainTransactionFragment

--- app/src/main/java/com/example/bitspender/presentation/transaction/MainTransactionFragment.kt
+++ app/src/main/java/com/example/bitspender/presentation/transaction/MainTransactionFragment.kt
@@ -1,9 +1,11 @@
 package com.example.bitspender.presentation.transaction
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -12,9 +14,12 @@ import com.example.bitspender.databinding.FragmentMainTransactionBinding
 import com.example.bitspender.di.utils.Injectable
 import com.example.bitspender.presentation.base.BaseFragment
 import com.example.bitspender.presentation.transaction.adapter.TransactionAdapter
+import com.example.bitspender.presentation.transaction.replenishbalance.ReplenishBalanceDialogFragment
+import com.example.bitspender.presentation.utils.observeWithLifecycle
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 class MainTransactionFragment : BaseFragment<FragmentMainTransactionBinding>(), Injectable {
-
 
     override val layoutResId: Int = R.layout.fragment_main_transaction
 
@@ -29,6 +34,7 @@ class MainTransactionFragment : BaseFragment<FragmentMainTransactionBinding>(), 
 
         setUpOnClickListeners()
         setUpAdapter()
+        setupObserver()
     }
 
     private fun setUpAdapter() {
@@ -47,7 +53,38 @@ class MainTransactionFragment : BaseFragment<FragmentMainTransactionBinding>(), 
             btnAddTransaction.setOnClickListener {
                 findNavController().navigate(MainTransactionFragmentDirections.actionTransactionsFragmentToAddTransactionFragment())
             }
+
+            btnReplenish.setOnClickListener {
+                openReplenishDialog()
+            }
         }
     }
 
+    private fun openReplenishDialog() {
+        ReplenishBalanceDialogFragment().show(
+            childFragmentManager,
+            ReplenishBalanceDialogFragment::class.simpleName
+        )
+    }
+
+    private fun setupObserver() {
+        viewModel.uiStateFlow.observeWithLifecycle(
+            viewLifecycleOwner,
+            observer = ::handleUiState
+        )
+
+        lifecycleScope.launch {
+            viewModel.transactions.collectLatest {
+                Log.d("anarion", "Submitting to adapter")
+                adapter.submitData(it)
+            }
+        }
+    }
+
+    private fun handleUiState(state: TransactionScreenState) {
+        binding.btnAddTransaction.isEnabled = state.isAddTransactionEnabled
+        binding.tvBalance.text = state.currentBalance.toString()
+        binding.tvBtcRate.text = state.btcRateState
+
+    }
 }
